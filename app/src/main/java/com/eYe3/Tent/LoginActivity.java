612c06a80ab27f3e0c0d7abf3e17f4b0d09c29cf
santhossunthar <santhoshsunthar@gmail.com>
2021-08-30 20:32:36 +0530

    Added major changes in login activity

--- app/src/main/java/com/eYe3/Tent/LoginActivity.java
+++ app/src/main/java/com/eYe3/Tent/LoginActivity.java
@@ -1,13 +1,65 @@
 package com.eYe3.Tent;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText userMail, userPassword;
+    private Button logBtn;
+    private FirebaseAuth mAuth;
+    private TextView createAccount;
+    private TextView forgotPass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userMail = findViewById(R.id.user_mail);
+        userPassword = findViewById(R.id.user_password);
+        logBtn = findViewById(R.id.log_btn);
+        mAuth = FirebaseAuth.getInstance();
+        forgotPass=findViewById(R.id.forgot_password);
+        forgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo: Setup password activity intent
+            }
+        });
+
+        final ProgressDialog progressDialog=new ProgressDialog(LoginActivity.this);
+
+        logBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo: Setup login button on click event
+            }
+        });
+
+        createAccount=findViewById(R.id.create_account);
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo: Setup register activity intent
+            }
+        });
+    }
+
+    private void showMessage(String text) {
+        Toast.makeText(getApplicationContext(),text,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
 
