09a591e1bca94d87c423233b37f5538ef64ac853
santhossunthar <santhoshsunthar@gmail.com>
2021-09-03 21:13:24 +0530

    Added minor changes in user activity

--- app/src/main/java/com/eYe3/Tent/UserActivity.java
+++ app/src/main/java/com/eYe3/Tent/UserActivity.java
@@ -2,7 +2,7 @@ package com.eYe3.Tent;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;i;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
