c9c6346f97ca5a990437b061e7e49883c5148afd
santhossunthar <santhoshsunthar@gmail.com>
2021-09-07 11:38:22 +0530

    Implemented login activity

--- app/src/main/java/com/eYe3/Tent/LoginActivity.java
+++ app/src/main/java/com/eYe3/Tent/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.eYe3.Tent;
 
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +10,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText userMail, userPassword;
@@ -16,6 +23,7 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private TextView createAccount;
     private TextView forgotPass;
+    private Intent mainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,13 +65,37 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        mainActivity=new Intent(this,MainActivity.class);
+
         createAccount=findViewById(R.id.create_account);
         createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // todo: Setup register activity intent
+                Intent registeractivity = new Intent(getApplicationContext(),RegisterActivity.class);
+                startActivity(registeractivity);
+                finish();
             }
         });
+    }
+
+    private void signIn(String mail, String password) {
+
+        mAuth.signInWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    updateUI();
+                }
+                else {
+                    showMessage(task.getException().getMessage());
+                }
+            }
+        });
+    }
+
+    private void updateUI() {
+        startActivity(mainActivity);
+        finish();
     }
 
     private void showMessage(String text) {
@@ -73,6 +105,20 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user!=null) {
+            user.reload();
+            boolean eMailVf = user.isEmailVerified();
+            if (eMailVf == true)
+            {
+                updateUI();
+            }else
+            {
+                // todo: Setup mail activity intent
+            }
+        }
     }
 }
 
